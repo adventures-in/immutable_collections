b5a836a5347372113b8abc90c58bd53c18725bd4
Nick Meinhold <nick.meinhold@gmail.com>
2021-03-14 23:31:18 +1100

    Add ImmutableMap

---
+++ lib/src/immutable_map.dart
@@ -0,0 +1,15 @@
+class ImmutableMap<K, V> {
+  final Map<K, V> _map;
+
+  ImmutableMap() : _map = {};
+  ImmutableMap.from(Map<K, V> map) : _map = {...map};
+
+  ImmutableMap._(Map<K, V> map) : _map = map;
+
+  ImmutableMap<K, V> copyAndAdd({required K key, required V value}) =>
+      ImmutableMap._({..._map}..addEntries([MapEntry(key, value)]));
+  ImmutableMap<K, V> copyAndRemove({required K key}) =>
+      ImmutableMap._({..._map}..remove(key));
+
+  factory ImmutableMap.fromJson(Map<K, V> json) => ImmutableMap.from(json);
+}
